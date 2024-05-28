6a3ce93f72d91a166f50910599e7324c8222bfb1
Matthias Rueedlinger <md@rueedlinger.ch>
2024-05-28 22:20:48 +0200

    Feat/docs (#7)
    * feat:  github action mkdocs, pytest and deploy docker

--- teal/libreoffice.py
+++ teal/libreoffice.py
@@ -12,41 +12,157 @@ _logger = logging.getLogger("teal.libreoffice")
 
 
 class LibreOfficeAdapter:
-    def __init__(self, libreoffice_cmd='soffice'):
+    def __init__(self, libreoffice_cmd="soffice"):
         self.libreoffice_cmd = libreoffice_cmd
         # copied from gotenberg, not sure if all are supported
-        self.supported_file_extensions = ['.123', '.602', '.abw', '.bib', '.bmp', '.cdr', '.cgm', '.cmx', '.csv',
-                                          '.cwk', '.dbf', '.dif', '.doc', '.docm', '.docx', '.dot', '.dotm', '.dotx',
-                                          '.dxf', '.emf', '.eps', '.epub', '.fodg', '.fodp', '.fods', '.fodt', '.fopd',
-                                          '.gif', '.htm', '.html', '.hwp', '.jpeg', '.jpg', '.key', '.ltx', '.lwp',
-                                          '.mcw', '.met', '.mml', '.mw', '.numbers', '.odd', '.odg', '.odm', '.odp',
-                                          '.ods', '.odt', '.otg', '.oth', '.otp', '.ots', '.ott', '.pages', '.pbm',
-                                          '.pcd', '.pct', '.pcx', '.pdb', '.pdf', '.pgm', '.png', '.pot', '.potm',
-                                          '.potx', '.ppm', '.pps', '.ppt', '.pptm', '.pptx', '.psd', '.psw', '.pub',
-                                          '.pwp', '.pxl', '.ras', '.rtf', '.sda', '.sdc', '.sdd', '.sdp', '.sdw',
-                                          '.sgl', '.slk', '.smf', '.stc', '.std', '.sti', '.stw', '.svg', '.svm',
-                                          '.swf', '.sxc', '.sxd', '.sxg', '.sxi', '.sxm', '.sxw', '.tga', '.tif',
-                                          '.tiff', '.txt', '.uof', '.uop', '.uos', '.uot', '.vdx', '.vor', '.vsd',
-                                          '.vsdm', '.vsdx', '.wb2', '.wk1', '.wks', '.wmf', '.wpd', '.wpg', '.wps',
-                                          '.xbm', '.xhtml', '.xls', '.xlsb', '.xlsm', '.xlsx', '.xlt', '.xltm',
-                                          '.xltx', '.xlw', '.xml'
-                                          ]
+        self.supported_file_extensions = [
+            ".123",
+            ".602",
+            ".abw",
+            ".bib",
+            ".bmp",
+            ".cdr",
+            ".cgm",
+            ".cmx",
+            ".csv",
+            ".cwk",
+            ".dbf",
+            ".dif",
+            ".doc",
+            ".docm",
+            ".docx",
+            ".dot",
+            ".dotm",
+            ".dotx",
+            ".dxf",
+            ".emf",
+            ".eps",
+            ".epub",
+            ".fodg",
+            ".fodp",
+            ".fods",
+            ".fodt",
+            ".fopd",
+            ".gif",
+            ".htm",
+            ".html",
+            ".hwp",
+            ".jpeg",
+            ".jpg",
+            ".key",
+            ".ltx",
+            ".lwp",
+            ".mcw",
+            ".met",
+            ".mml",
+            ".mw",
+            ".numbers",
+            ".odd",
+            ".odg",
+            ".odm",
+            ".odp",
+            ".ods",
+            ".odt",
+            ".otg",
+            ".oth",
+            ".otp",
+            ".ots",
+            ".ott",
+            ".pages",
+            ".pbm",
+            ".pcd",
+            ".pct",
+            ".pcx",
+            ".pdb",
+            ".pdf",
+            ".pgm",
+            ".png",
+            ".pot",
+            ".potm",
+            ".potx",
+            ".ppm",
+            ".pps",
+            ".ppt",
+            ".pptm",
+            ".pptx",
+            ".psd",
+            ".psw",
+            ".pub",
+            ".pwp",
+            ".pxl",
+            ".ras",
+            ".rtf",
+            ".sda",
+            ".sdc",
+            ".sdd",
+            ".sdp",
+            ".sdw",
+            ".sgl",
+            ".slk",
+            ".smf",
+            ".stc",
+            ".std",
+            ".sti",
+            ".stw",
+            ".svg",
+            ".svm",
+            ".swf",
+            ".sxc",
+            ".sxd",
+            ".sxg",
+            ".sxi",
+            ".sxm",
+            ".sxw",
+            ".tga",
+            ".tif",
+            ".tiff",
+            ".txt",
+            ".uof",
+            ".uop",
+            ".uos",
+            ".uot",
+            ".vdx",
+            ".vor",
+            ".vsd",
+            ".vsdm",
+            ".vsdx",
+            ".wb2",
+            ".wk1",
+            ".wks",
+            ".wmf",
+            ".wpd",
+            ".wpg",
+            ".wps",
+            ".xbm",
+            ".xhtml",
+            ".xls",
+            ".xlsb",
+            ".xlsm",
+            ".xlsx",
+            ".xlt",
+            ".xltm",
+            ".xltx",
+            ".xlw",
+            ".xml",
+        ]
 
     def convert_to_pdf(self, data, filename) -> FileResponse | JSONResponse:
 
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in self.supported_file_extensions:
-            return create_json_err_response(400, f"file extension '{file_ext}' is not supported ({filename}).")
+            return create_json_err_response(
+                400, f"file extension '{file_ext}' is not supported ({filename})."
+            )
 
         # create tmp dir for all files
-        tmp_dir = tempfile.mktemp(prefix='teal-')
+        tmp_dir = tempfile.mktemp(prefix="teal-")
         _logger.debug(f"creating tmp dir: {tmp_dir}")
         os.mkdir(tmp_dir)
 
         tmp_file_in_path = os.path.join(tmp_dir, f"tmp{file_ext}")
         tmp_out_dir = os.path.join(tmp_dir, "out")
 
-        with open(tmp_file_in_path, 'wb') as tmp_file_in:
+        with open(tmp_file_in_path, "wb") as tmp_file_in:
             _logger.debug(f"writing file {filename} to {tmp_file_in_path}")
             tmp_file_in.write(data)
 
@@ -54,21 +170,36 @@ class LibreOfficeAdapter:
         cmd_convert_pdf = f'{self.libreoffice_cmd} --headless --convert-to pdf --outdir "{tmp_out_dir}" "{tmp_file_in_path}"'
 
         _logger.debug(f"running cmd: {cmd_convert_pdf}")
-        result = subprocess.run(cmd_convert_pdf, shell=True, capture_output=True, text=True, env={'HOME': '/tmp'})
+        result = subprocess.run(
+            cmd_convert_pdf,
+            shell=True,
+            capture_output=True,
+            text=True,
+            env={"HOME": "/tmp"},
+        )
 
         converted_file_out = os.path.join(tmp_dir, "out", "tmp.pdf")
 
         if result.returncode == 0:
             if os.path.exists(converted_file_out):
-                return FileResponse(converted_file_out, media_type='application/pdf',
-                                    filename=f"{os.path.splitext(filename)[0]}.pdf",
-                                    background=BackgroundTask(cleanup_tmp_dir, tmp_dir))
+                return FileResponse(
+                    converted_file_out,
+                    media_type="application/pdf",
+                    filename=f"{os.path.splitext(filename)[0]}.pdf",
+                    background=BackgroundTask(cleanup_tmp_dir, tmp_dir),
+                )
 
             else:
                 _logger.debug(f"file was not written {result}")
-                return create_json_err_response(500, f"could not convert file '{filename}' {result.stderr}",
-                                                background=BackgroundTask(cleanup_tmp_dir, tmp_dir))
+                return create_json_err_response(
+                    500,
+                    f"could not convert file '{filename}' {result.stderr}",
+                    background=BackgroundTask(cleanup_tmp_dir, tmp_dir),
+                )
         else:
             _logger.debug(f"cmd was not successful {result}")
-            return create_json_err_response(500, f"got return code {result.returncode} '{filename}' {result.stderr}",
-                                            background=BackgroundTask(cleanup_tmp_dir, tmp_dir))
+            return create_json_err_response(
+                500,
+                f"got return code {result.returncode} '{filename}' {result.stderr}",
+                background=BackgroundTask(cleanup_tmp_dir, tmp_dir),
+            )
