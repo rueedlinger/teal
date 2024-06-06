603ff6e7e0943d56edc5c28984e013539a278ecc
Matthias Rueedlinger <md@rueedlinger.ch>
2024-06-06 22:06:41 +0200

    feat: update libreoffice version (use debian backports) #25

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,10 +2,10 @@ from teal.model import LibreOfficePdfProfile, OcrPdfAProfile
 
 
 def test_libreoffice_pdf_profile_model():
-    # assert LibreOfficePdfProfile.PDF17.to_libreoffice_pdf_version() == "17"
+    assert LibreOfficePdfProfile.PDF17.to_libreoffice_pdf_version() == "17"
     assert LibreOfficePdfProfile.PDF_16.to_libreoffice_pdf_version() == "16"
     assert LibreOfficePdfProfile.PDF_15.to_libreoffice_pdf_version() == "15"
-    assert LibreOfficePdfProfile.PDFA_1a.to_libreoffice_pdf_version() == "1"
+    assert LibreOfficePdfProfile.PDFA_1B.to_libreoffice_pdf_version() == "1"
     assert LibreOfficePdfProfile.PDFA_2B.to_libreoffice_pdf_version() == "2"
     assert LibreOfficePdfProfile.PDFA_3B.to_libreoffice_pdf_version() == "3"
 
